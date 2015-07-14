715c489fc406c45b4db4deef15a5530adb5d45e1
Neal Jackson <neal.jackson@intel.com>
2015-07-14 16:54:28 -0700

    allows read of characteristics

--- oad.py
+++ oad.py
@@ -5,7 +5,8 @@ import argparse
 sys.path.append('../bluepy/bluepy/')
 from btle import UUID, Peripheral, DefaultDelegate
 
-lescan_timeout = 5
+# global
+LESCAN_TIMEOUT = 5
 procs = []
 
 def sigint_handler(sig, frame):
@@ -15,6 +16,9 @@ def sigint_handler(sig, frame):
     print('\n')
     sys.exit(0)
 signal.signal(signal.SIGINT, sigint_handler)
+
+def _TI_UUID(val):
+    return UUID("%08X-0451-4000-B000-000000000000" % (0xF0000000+val))
 
 def main(argv):
     # check if root
@@ -45,7 +49,7 @@ def do_lescan():
     # add to list of active subprocesses
     procs.append(proc)
     while(True):
-        time.sleep(lescan_timeout)
+        time.sleep(LESCAN_TIMEOUT)
         # pause scanning
         proc.send_signal(signal.SIGSTOP)
 
@@ -67,9 +71,20 @@ def do_lescan():
     procs.remove(proc)
 
 def make_peripheral(address):
-    peripheral = Peripheral(address)
-    services = peripheral.getServices()
-    print(services)
-
+    try:
+        peripheral = Peripheral(address)
+    except Exception:
+        print('failed to open peripheral')
+    characs = peripheral.getCharacteristics()
+    charac_dir = {}
+    for charac in characs:
+        charac_dir[format(charac.getHandle(), '02x')] = charac
+        print(format(charac.getHandle(), '02x'))
+    while(True):
+        to_read = input('characteristic to read: ')
+        try:
+            print(charac_dir[to_read].read())
+        except Exception:
+            print('failed to read')
 if __name__ == "__main__":
     main(sys.argv)
