120858972b641ccb6334d8dd78f8d47b57fcd42d
Neal Jackson <neal.jackson@intel.com>
2015-07-14 11:05:14 -0700

    Added optional argument for hci scans, moved ble scanning into function. scanning can be repeated after each timeout

--- oad.py
+++ oad.py
@@ -21,16 +21,50 @@ def main(argv):
     if not os.geteuid() == 0:
         sys.exit('must be root')
 
-    proc = subprocess.Popen(['/usr/bin/hcitool', 'lescan'])
-    procs.append(proc)
-    time.sleep(lescan_timeout)
-    proc.send_signal(signal.SIGINT)
-    procs.remove(proc)
+    # add command line option for scanning
+    parser = argparse.ArgumentParser(description='An OAD tool for TI CC26xx MCUs')
+    parser.add_argument('-s', '--scan', action='store_true',
+                        help='provides an initial HCI scan to determine target address')
+
+    args = parser.parse_args()
+    if(args.scan):
+        do_lescan()
 
     remote_address = input('address of target device: ')
     print(remote_address)
     make_peripheral(remote_address)
 
+def do_lescan():
+    # make sure hcitool is installed, and launch subprocess
+    try:
+        proc = subprocess.Popen(['/usr/bin/hcitool', 'lescan'])
+    except Exception:
+        print('hcitool is not installed, install bluez')
+        sys.exit(1)
+
+    # add to list of active subprocesses
+    procs.append(proc)
+    while(True):
+        time.sleep(lescan_timeout)
+        # pause scanning
+        proc.send_signal(signal.SIGSTOP)
+
+        # ask to continue
+        while(True):
+            more_scan = input('continue scanning? (y/n) ').strip()
+            if(more_scan == 'y' or more_scan == 'n'):
+                break
+        # continue subprocess regardless
+        # if we get an SIGINT here, we want to let hcitool exit gracefully
+        proc.send_signal(signal.SIGCONT)
+        if(more_scan.strip() == 'n'):
+            break
+        else:
+            continue
+
+    # tell hcitool to exit and remove from active subprocess list
+    proc.send_signal(signal.SIGINT)
+    procs.remove(proc)
 
 def make_peripheral(address):
     peripheral = Peripheral(address)
