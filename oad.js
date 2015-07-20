a79ef2c96255be0f9dfceed102a791a8578be17a
Neal Jackson <neal.jackson@intel.com>
2015-07-20 16:47:51 -0700

    added CRC calculations

--- oad.js
+++ oad.js
@@ -3,17 +3,26 @@ var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
 var async = require('async');
 
+//console.dir(argv);
 
+var OAD_SERVICE = 'f000ffc004514000b000000000000000';
+var IMG_IDENTIFY_CHARACTERISTIC = 'f000ffc104514000b000000000000000';
+var IMG_BLOCK_CHARACTERISTIC = 'f000ffc204514000b000000000000000';
 var OAD_BLOCK_SIZE = 16;
 var OAD_BUFFER_SIZE = 2 + OAD_BLOCK_SIZE;
 
+var CRC_POLY = 0x1021;
+var BYTE_MASK = 0xFF;
+var SHORT_MASK = 0xFFFF;
 
-console.dir(argv);
+// image variables
+var img_header = new Buffer(12);
 
+// ble connection variables
 var target_uuid = null;
 var scan_timeout = null;
 var target_device = null;
-var discovered = new Array();
+//var discovered = new Array();
 
 // Input args
 if(argv.h)
@@ -41,7 +50,7 @@ init();
 function init(){
   fs.readFile(argv.f, function (err, data) {
     if (err) throw err;
-    console.log(data.length);
+    prepare_image(data);
     noble.on('discover', discover_device);
   });
 
@@ -59,6 +68,79 @@ function init(){
         noble.stopScanning();
       }
   });
+}
+
+function prepare_image(data)
+{
+  data.copy(img_header, 0, 0x4, 0x10);
+  console.log(img_header);
+  var crc = crc16(0x0, 0x37);
+  crc = crc16(crc, 0x00);
+  crc = crc16(crc, 0x00);
+  crc = crc16(crc, 0x00);
+  console.log(crc.toString(16));
+}
+
+function calc_image_crc(page, buf){
+  var crc = 0x00;
+  var addr = page * 0x1000;
+
+  var pageBeg = page & SHORT_MASK;
+  var pageEnd = (buf.length / (0x1000 / 4)) & BYTE_MASK;
+  var osetEnd = ((buf.length - (pageEnd * (0x1000 / 4))) * 4);
+
+  pageEnd += pageBeg;
+
+  while(true) {
+    for(var oset = 0; oset < 0x1000; oset++) {
+      if((page === pageBeg) && (oset === 0x00)) {
+        // Skip the CRC and CRC shadow.
+        // CRC and CRC shadow are each 2 bytes, so 4 bytes total
+        // Skip three because we increment one more time on next loop.
+
+        oset += 3;
+      }
+      else if ((page === pageEnd) && (oset === osetEnd)) {
+        crc = crc16(crc, 0x00);
+        crc = crc16(crc, 0x00);
+
+        return crc;
+      }
+      else {
+        crc = crc16(crc, buf[(addr + oset)]);
+      }
+    }
+    page += 1;
+    addr = page * 0x1000;
+  }
+}
+
+function crc16(crc, val) {
+  val &= BYTE_MASK;
+  crc &= SHORT_MASK;
+
+  for(var cnt = 0; cnt < 8; cnt++, val <<= 1)
+  {
+    var msb;
+    val &= BYTE_MASK;
+
+    if((crc & 0x8000) == 0x8000) {
+      msb = 1;
+    }
+    else msb = 0;
+
+    crc <<= 1;
+    crc &= SHORT_MASK;
+
+    if((val & 0x80) == 0x80) {
+      crc |= 0x0001;
+    }
+    if(msb == 1){
+      crc ^= CRC_POLY;
+    }
+  }
+
+  return crc &= SHORT_MASK;
 }
 
 function discover_device(peripheral)
@@ -102,16 +184,20 @@ function print_firmware(callback){
 
 function oad_program(){
   // OAD service
-  service_uuids = ['f000ffc004514000b000000000000000'];
+  service_uuids = [OAD_SERVICE];
   // Img Identify, Img Block
-  characteristic_uuids = ['f000ffc104514000b000000000000000', 'f000ffc204514000b000000000000000'];
+  characteristic_uuids = [IMG_IDENTIFY_CHARACTERISTIC, IMG_BLOCK_CHARACTERISTIC];
   target_device.discoverSomeServicesAndCharacteristics(service_uuids, characteristic_uuids,
   function(err, services, characteristics){
 
     chars_servs_exist(err, services, characteristics);
-
+    img_identify_char = characteristics[0];
+    img_block_char = characteristics[1];
     console.log('programming device ' + target_uuid.match(/../g).join(':'));
+
     //TODO actually program
+
+    target_device.disconnect();
   });
   target_device.disconnect();
 }
